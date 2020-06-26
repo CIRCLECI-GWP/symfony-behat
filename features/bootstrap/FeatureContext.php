0368669307147c638f7363535295bbdce8da091f
Ndagi Stanley <ndagis@gmail.com>
2020-06-27 00:53:38 +0300

    Add missing steps

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -3,6 +3,8 @@
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
+use Symfony\Component\HttpClient\HttpClient;
 
 /**
  * Defines application features from the specific context.
@@ -18,5 +20,30 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+
+    /**
+     * @Given I am an unauthenticated user
+     */
+    public function iAmAnUnauthenticatedUser()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I request a list of customers from :arg1
+     */
+    public function iRequestAListOfCustomersFrom($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then The results should include a customer with ID :arg1
+     */
+    public function theResultsShouldIncludeACustomerWithId($arg1)
+    {
+        throw new PendingException();
     }
 }
